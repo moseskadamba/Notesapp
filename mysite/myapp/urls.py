85a1f633cafda0d9d3a4aee7e2011ce812af7049
moses kadamba <moseskadamba@gmail.com>
2025-05-07 17:42:08 +0300

    I added a few featuree like the dashboard for portfolio

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.dashboard, name='dashboard'),
+    path('home/', views.home, name='home'),
     path('add/', views.add_note, name='add_note'),
     path('edit/<int:note_id>/', views.edit_note, name='edit_note'),
     path('delete/<int:note_id>/', views.delete_note, name='delete_note'),
